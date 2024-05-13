5c4a9d33dbe15ec75ade8e6a8ff356df55d0e0dc
Jack Davis <jack@planes.agency>
2024-05-13 14:53:26 +0100

    Remove redirect from staff only page

--- pages/staff-only.tsx
+++ pages/staff-only.tsx
@@ -10,19 +10,19 @@ export default function StaffOnly() {
   const { data: session, status } = useSession();
   const { push } = useRouter();
 
-  if (
-    status !== "loading" &&
-    !session?.user.roles.includes(env.NEXT_PUBLIC_STAFF_GROUP_ID)
-  ) {
-    push("/");
-  }
-
   return (
     <Box w="75%" maxW="420px" m="0 auto">
-      <Heading my={8}>Staff Only 🚨</Heading>
-      <Text mb={6} fontSize="lg">
-        Welcome to the staff portal!
-      </Text>
+      <Heading my={8}>Staff Portal</Heading>
+      {status !== "loading" &&
+      session?.user.roles.includes(env.NEXT_PUBLIC_STAFF_GROUP_ID) ? (
+        <Text mb={6} fontSize="lg">
+          Welcome to the staff portal 👋
+        </Text>
+      ) : (
+        <Text mb={6} fontSize="lg">
+          Access Denied ❌
+        </Text>
+      )}
     </Box>
   );
 }
